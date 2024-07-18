d7a42a88a28499e8bdcf98c8a34a23cf78776b85
LC <1243qw6@gmail.com>
2024-07-18 12:04:02 +0900

    0718

--- JS/ch05.함수/01.why.js
+++ JS/ch05.함수/01.why.js
@@ -9,7 +9,9 @@
 
 function isPrimeNumber(num) {
     let sqrtNum = Math.floor(Math.sqrt(num));
-    for ( let i = 2; i <= sqrtNum; i++)
+        for ( let i = 2; i <= sqrtNum; i++) {
         if (num % i == 0)
             return false;
-}
+        }
+        return true;
+    }
