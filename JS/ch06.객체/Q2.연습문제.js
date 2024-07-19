8e0348c8137fbac3e12f21b8eb0b089da5a15e13
LC <1243qw6@gmail.com>
2024-07-19 15:59:13 +0900

    0719

--- JS/ch06.객체/Q2.연습문제.js
+++ JS/ch06.객체/Q2.연습문제.js
@@ -33,7 +33,6 @@ console.log(practice.substring(33));
 //    가장 큰 palindrome 수와 어떤 수를 곱해서 나온 결과인가?
 
 
-
 // 4. 다음 문장에서 the 단어는 몇번 사용되었는가?
 let sample = `the grown-ups' response, this time, was to advise me to lay aside my drawings of 
 boa constrictors, whether from the inside or the outside, and devote myself instead to geography, 
@@ -41,6 +40,4 @@ history, arithmetic, and grammar. That is why, at the, age of six, I gave up wha
 magnificent career as a painter. I had been disheartened by the failure of my Drawing Number One and 
 my Drawing Number Two. Grown-ups never understand anything by themselves, and it is tiresome for children 
 to be always and forever explaining things to the.`;
-
-
 
